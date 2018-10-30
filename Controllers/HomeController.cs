0c950d0057f268e05727f4cbc98822a9b41d3684
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-10-30 13:21:58 -0700

    Wire Frame and Gallery Changes
    Changes to wire frame and adding Vic personal gallery photos.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,28 +10,11 @@ namespace VicCV.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //Root
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index(){
+            return View("Index");
+        }   
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,30 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function(){
+
+    //Lock custom navbar when scrolled.
+    $(window).scroll(function() {
+        var distanceFromTop = $(document).scrollTop();
+        if (distanceFromTop >= $('#Navbar').height())
+        {
+            $('#Navbar').addClass('custom_navbar');
+        }
+        else
+        {
+            $('#Navbar').removeClass('custom_navbar');
+        }
+    });
+
+    //Show Hidden Gallery Full Size Display on Img click.
+    $('img.img_moon_phase_thumbnail').on( "click", function() {
+        $('#fullscalegalleryimg').show();
+        var srcImg = $(this).attr("src");
+        var imgName = $(this).attr("alt");
+        $('#fullscalegalleryimg img').attr("src", srcImg);
+        $('#fullscalegalleryimg h1').html(imgName);
+        console.log(srcImg);
+      });
+    //Hidden Gallery Full Size Display on click.
+      $("#fullscalegalleryimg").click(function(){
+		$(this).hide();
+	});
+});
